6b539e4fd015293c63e1148808b5adc16eff4caa
Armando Arce <71146244+armando-arce@users.noreply.github.com>
2021-11-16 17:33:11 -0600

    Update login.js

--- netlify/functions/login.js
+++ netlify/functions/login.js
@@ -11,7 +11,7 @@ exports.handler = async (event, context) => {
 
   try {
 		
-		const { email, password } = event.body;
+		const { email, password } = JSON.parse(event.body);
 
     if (!(email && password)) {
       return { statusCode: 400, headers, body:"All input is required"};
