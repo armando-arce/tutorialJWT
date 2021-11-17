39d27cc8151b1c3ea8c9f90b97a206517693a866
Armando Arce <71146244+armando-arce@users.noreply.github.com>
2021-11-16 20:01:43 -0600

    Update login.js

--- netlify/functions/login.js
+++ netlify/functions/login.js
@@ -18,18 +18,18 @@ exports.handler = async (event, context) => {
       return { statusCode: 400, headers, body:"All input is required"};
     }
 
-    let user = {'_id':1,'password':''};
-    user.password = await bcrypt.hash('12345', 10);
+    let user = {'_id':1,'token':''};
+    passwd = await bcrypt.hash('12345', 10);
 
-    if (await bcrypt.compare(data.password, user.password)) {
-      const token = jwt.sign(
+    if (await bcrypt.compare(data.password, passwd)) {
+      user.token = jwt.sign(
          { user_id: user._id, email:data.email },
          process.env.TOKEN_KEY,
          {
            expiresIn: "2h",
          }
        );
-      return { statusCode: 200, headers, body: token};
+      return { statusCode: 200, headers, body: JSON.stringify(user)};
     }
 		return { statusCode: 400, headers, body: 'Invalid Credentials' };
   } catch (error) {
